0f5a57d009081dbdc6301a59dacb0a18471b4ac5
Dong Nguyen <dongnv@vmodev.com>
2022-03-10 18:59:28 +0700

    add

---
+++ pages/api/about.ts
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export interface AboutPage;
+
+export default function About() {
+    return (
+        <div>
+            About page
+        </div>
+    )
+}
---
+++ public/static/AUIGrid/AUIGridLicense.js
@@ -0,0 +1,9 @@
+/*!
+ * AUIGrid 3.0.11 License
+ * License Type : Evaluation(Trial) Version
+ * Authorized Domain(or IP) : localhost,127.0.0.1
+ * Expiration Date : 2022-04-09
+ * www.auisoft.net
+ */
+ /* eslint-disable */
+AUIGridLicense = "eyJjdCI6IjNRaHZjSTduY2psNzJsSXgrM01sY3g2cWlDa0tCelwvbDFlZmlQU0ZhXC9VR3JBWnFVOFJPS0RlRTM1cWJZUzM3QXhGUEpIMER0b0V4WXBQSmphUmhxeVhvUk8wOU1weUlHSmdudkM3d2RaTFU9IiwiaXYiOiJlNjk5ZTEwMmJmODA4NjVkYTY2ZDBmMTM3YzE2MGEyOCIsInMiOiIwNGFjODMwMjI3ZGE4ZjFlIn0=";
